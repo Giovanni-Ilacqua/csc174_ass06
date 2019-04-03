37e42e618b35282e1ed1a5685a1de4fc92dcf02c
definitelynotavirus <jdalton9@u.rochester.edu>
2019-04-02 21:44:57 -0400

    Fixed Thank you

--- thank-you.php
+++ thank-you.php
@@ -1,3 +1,36 @@
+<?php
+	// 1. Create a database connection
+	require_once("connect-db.php");
+
+	
+	// these are from the text INPUT fields...
+	$firstName = Trim(stripslashes($_POST['firstName'])); 
+	$lastName = Trim(stripslashes($_POST['lastName'])); 
+	$email = Trim(stripslashes($_POST['email'])); 
+	
+	
+	// You really should escape all strings
+	//$insertData = mysqli_real_escape_string($connection, $insertData);
+		// Repeat the above line as needed
+	
+	// 2. Perform database query
+	$query  = "INSERT INTO subscribers (first_name, last_name, email) VALUES ('$firstName','$lastName','$email')";
+	$result = mysqli_query($connection, $query);
+
+?>
+
+
+
+
+<?php
+	// 4. Step 4 is unnecessary here because we didn't 
+	//	  get data that needs to be released
+	//mysqli_free_result($result);
+
+	// 5. Close database connection
+	mysqli_close($connection);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
