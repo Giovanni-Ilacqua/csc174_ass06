dbaf3d7d39d06bf1806afb5cf97c15d0cbab5e4c
definitelynotavirus <jdalton9@u.rochester.edu>
2019-04-02 21:21:16 -0400

    Updated Titles

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Giodme Personal Website</title>
+    <title>Assignment 06</title>
     <?php include 'include/html-links.inc'; ?>
 </head>
 
